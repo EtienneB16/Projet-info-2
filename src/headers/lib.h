ba79958fc1f5d69fb28cf663e3cfa8a01b5845fa
Etienne Bellec <etienne.bellec@edu.ece.fr>
2026-04-10 13:25:01 +0200

    test

---
+++ src/headers/lib.h
@@ -0,0 +1,16 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro.h>
+#include <time.h>
+
+typedef struct {
+    int x;
+    int y;
+    int vies;
+    int score;
+}Joueur;
+
+#endif
