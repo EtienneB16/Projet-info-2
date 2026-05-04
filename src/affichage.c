5e115c37158aae44853fcdc36f8d4c05007b827a
cvsperrier <cvs.perrier@gmail.com>
2026-05-04 15:52:43 +0200

    Update affichage.c

--- src/affichage.c
+++ src/affichage.c
@@ -2,7 +2,16 @@
 #include "headers/affichage.h"
 #include "headers/partie.h"
 
-void afficherMenu(BITMAP* menu){
-    stretch_blit(menu, screen, 0, 0, menu->w, menu->h, 0, 0, LARGEUR, HAUTEUR);
+void afficher_menu(BITMAP* bg, BITMAP* page){
+    stretch_blit(bg, page , 0, 0, bg->w, bg->h, 0, 0, LARGEUR, HAUTEUR);
+    rectfill(page, 300, 150, 500, 200, makecol(255, 255, 255));
+    rectfill(page, 300, 225, 500, 275, makecol(255, 255, 255));
+    rectfill(page, 300, 300, 500, 350, makecol(255, 255, 255));
+    rectfill(page, 300, 375, 500, 425, makecol(255, 255, 255));
+    textout_centre_ex(page, font, "Jouer", 400, 175, makecol(0, 0, 0), -1);
+    textout_centre_ex(page, font, "Reprendre une partie", 400, 250, makecol(0, 0,0), -1);
+    textout_centre_ex(page, font, "Régles", 400, 325, makecol(0, 0,0), -1);
+    textout_centre_ex(page, font, "Quitter", 400, 400, makecol(0, 0,0), -1);
+    blit(page, screen, 0, 0, 0, 0, LARGEUR, HAUTEUR);
+    show_mouse(screen);
 }
-
